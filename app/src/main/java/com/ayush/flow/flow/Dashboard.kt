073e09fca2d872f0cdaa62974d7a0c505afc9ffd
Ayush <am838578@gmail.com>
2021-07-02 03:38:58 +0530

    Updated Register.kt

---
+++ app/src/main/java/com/ayush/flow/flow/Dashboard.kt
@@ -0,0 +1,12 @@
+package com.ayush.flow.flow
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ayush.flow.R
+
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
---
+++ app/src/main/java/com/ayush/flow/flow/Splash.kt
@@ -0,0 +1,18 @@
+package com.ayush.flow.flow
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.ayush.flow.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, Register::class.java))
+        },1500)
+    }
+}
